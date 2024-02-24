75330ef61bf64504f4e595a70b7c3b7686248b5a
aditya <adityasingla505@gmail.com>
2024-02-24 18:22:41 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
@@ -13,17 +13,16 @@ def get_requirements(file_path:str)->List[str]:
         requirements=file_obj.readlines()
         requirements=[req.replace('\n',"") for req in requirements]
 
-        
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
 setup(
-    name='ML-Project',
-    version='0.0.1',
-    author='Aditya',
-    author_email='adityasingla505@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+name='mlproject',
+version='0.0.1',
+author='krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
 )
--- template.py
+++ template.py
@@ -4,26 +4,26 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-project_name = "mlproject"
+project_name="mlproject"
 
-list_of_files=[ 
-f"src/{project_name}/__init__.py",
-f"src/{project_name}/components/__init__.py",
-f"src/{project_name}/components/data_ingestion.py",
-f"src/{project_name}/components/data_transformation.py",
-f"src/{project_name}/components/model_trainer.py",
-f"src/{project_name}/components/model_monitering.py",
-f"src/{project_name}/pipelines/__init__.py",
-f"src/{project_name}/pipelines/training_pipeline.py",
-f"src/{project_name}/pipelines/prediction_pipeline.py",
-f"src/{project_name}/exception.py",
-f"src/{project_name}/logger.py",
-f"src/{project_name}/utils.py",
-f"main.py",
-f"app.py",
-f"Dockerfile",
-f"requirements.txt",
-f"setup.py"
+list_of_files=[
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/model_tranier.py",
+    f"src/{project_name}/components/model_monitering.py",
+    f"src/{project_name}/pipelines/__init__.py",
+    f"src/{project_name}/pipelines/training_pipeline.py",
+    f"src/{project_name}/pipelines/prediction_pipeline.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/utils.py",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py"
 ]
 
 for filepath in list_of_files:
@@ -31,15 +31,16 @@ for filepath in list_of_files:
     filedir, filename = os.path.split(filepath)
 
     if filedir != "":
-        os.makedirs(filedir,exist_ok=True)
+        os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory:{filedir} for the file {filename}")
 
+    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, 'w') as f:
+        with open(filepath,'w') as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
 
+    
     else:
         logging.info(f"{filename} is already exists")
-
