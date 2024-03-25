de1bc5f661b5026f4bf305bc28c69ec187575957
aditya <adityasingla505@gmail.com>
2024-03-25 11:05:34 +0530

    first commit

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ git add .
 git commit -m ""
 git push origin main
 python app.py
+
+MLFLOW_TRACKING_URI=https://dagshub.com/imavi-5/ML-Project.mlflow \
+MLFLOW_TRACKING_USERNAME=imavi-5 \
+MLFLOW_TRACKING_PASSWORD=d779b97386e19bded6986ba6a214e4f052b98a17 \
+python script.py
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
-
+from src.mlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 if __name__=="__main__":
     logging.info("The execution has started")
@@ -17,7 +17,11 @@ if __name__=="__main__":
 
         #data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        train_arr,test_arr,_ = data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        ## Model Training
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
     except Exception as e:
         logging.info("Custome Exception")
